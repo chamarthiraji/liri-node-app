d36eaaa4c7992a2c4f00dd8f94dd0b32b5c79b79
Rajyalakshmi C <chamarthi226@gmail.com>
2016-10-14 22:51:30 -0500

    added usage details function and did indentation

--- liri.js
+++ liri.js
@@ -9,9 +9,7 @@ var value= a[3];
 var songName='';
 var songNameArray=[];
 
-
 //getting twitter keys from key.js
-
 var client = new Twitter({
   consumer_key:twitterKey.consumer_key,
   consumer_secret: twitterKey.consumer_secret,
@@ -19,62 +17,87 @@ var client = new Twitter({
   access_token_secret: twitterKey.access_token_secret
   
 });
+
 //function to get tweets
 function myTweets(){
-			var params = {user_id: 'raji3324',count:3
-						 };
-								
+	var params = {user_id: 'raji3324',count:20
+				 };						
 
-			client.get('statuses/user_timeline', params, function(error, tweets, response) {
-				if (!error) {
-				  	tweets.forEach(function(item,index){
-				  		console.log("------my tweets------");
-				  		console.log("tweet"+index+": "+tweets[index].text);
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+		if (!error) {
+		  	console.log("------my tweets------");
+		  	tweets.forEach(function(item,index){
+		  		console.log("tweet"+index+": "+tweets[index].text);
 
-				  	})
-			  	}
-			});
+		  	})
+	  	}
+	});
 }
 
 //function to get song deatails from spotify
-function spotifyMySongs(value){
+function songDetails(songName){
+    spotify.search({ type: 'track' , query: songName}, function(err, data) {
+        if ( err ) {
+            console.log('Error occurred: ' + err);
+            return;
+        }else{
+            	var trackArray = data.tracks.items;
+            	trackArray.forEach(function(item,index){
+                    console.log("-------------"+trackArray[index].name+" Details----------------------");
+            		console.log("Album Name: "+trackArray[index].album.name);
+            		console.log("Track Name: "+ trackArray[index].name);
+            		console.log("Artist Name: "+trackArray[index].artists[0].name);
+            		console.log("Preview Url: "+trackArray[index].preview_url);
+            		console.log("-----------------------------------");
+                   });                   
+             }	        
+    });	        
+ 	
+}
 
-	function songDetails(songName){
-	    spotify.search({ type: 'track' , query: songName}, function(err, data) {
-	        if ( err ) {
-	            console.log('Error occurred: ' + err);
-	            return;
-	        }else{
-	            	var trackArray = data.tracks.items;
-	            	trackArray.forEach(function(item,index){
-	                    console.log("-------------"+trackArray[index].name+" Details----------------------");
-	            		console.log("Album Name: "+trackArray[index].album.name);
-	            		console.log("Track Name: "+ trackArray[index].name);
-	            		console.log("Artist Name: "+trackArray[index].artists[0].name);
-	            		console.log("Preview Url: "+trackArray[index].preview_url);
-	            		console.log("-----------------------------------");
-	                   });                   
-	             }	        
-	    });	        
-     	
-	}
+//function to get song deatails from spotify
+function spotifyMySongs(value) {
     
-	if(value === undefined){
+	if(value === undefined) {
 	   //"https://api.spotify.com/v1/search?query=track%3Athe+sign+artist%3AAce+of+Base&offset=0&limit=1&type=track",
 	    songName='track:the+sign+artist:Ace+of+Base&offset=0&limit=1'
 	   // console.log('songName'+songName);
 	    songDetails(songName);	    
 
-	}else{ 
+	} else { 
 	    songName = value+'&limit=1';
 	    songDetails(songName);
 	}
 }
 
 
+function movieInfo(movieName){
+	var queryUrl = 'http://www.omdbapi.com/?t=' + movieName +'&y=&plot=short&r=json&tomatoes=true';
+
+
+	// Then create a request to the queryUrl
+	request(queryUrl,function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			//console.log(body);
+			// console.log(JSON.stringify(body, null, 2));
+			console.log("**************  "+ JSON.parse(body).Title+" Details ******************")
+			console.log("Title : "+JSON.parse(body).Title); 
+			console.log("Year : "+JSON.parse(body).Year); 
+			console.log("IMDB Rating : "+JSON.parse(body).imdbRating); 
+			console.log("Country where the movie was produced : "+JSON.parse(body).Country); 
+			console.log("Language : "+JSON.parse(body).Language); 
+			console.log("Plot : "+JSON.parse(body).Plot); 
+			console.log("Actors : "+JSON.parse(body).Actors); 
+			console.log("Rotten Tomatoes Rating : "+JSON.parse(body).tomatoRating); 
+			console.log("Rotten Tomatoes URL : "+JSON.parse(body).tomatoURL); // Show the HTML for the Google homepage. 
+		}
+	});
+}
+
 //function to get movie details from IMDB node package
-function movieThis(value){
+function movieThis(value) {
 	var movieName="" ;
+
 	if(value === undefined){
 		movieName += "Mr.Nobody";
 		//console.log('movieName'+movieName);
@@ -86,34 +109,12 @@ function movieThis(value){
 		movieName = value;
 		movieInfo(movieName);
 	}
+
 	// Then run a request to the OMDB API with the movie specified 
 	var queryUrl = 'http://www.omdbapi.com/?t=' + movieName +'&y=&plot=short&r=json&tomatoes=true';
 
 	// This line is just to help us debug against the actual URL.  
 	//console.log(queryUrl);
-
-	function movieInfo(movieName){
-		var queryUrl = 'http://www.omdbapi.com/?t=' + movieName +'&y=&plot=short&r=json&tomatoes=true';
-
-
-		// Then create a request to the queryUrl
-		request(queryUrl,function (error, response, body) {
-		  if (!error && response.statusCode == 200) {
-		   //console.log(body);
-		  // console.log(JSON.stringify(body, null, 2));
-		  	console.log("**************  "+ JSON.parse(body).Title+" Details ******************")
-		    console.log("Title : "+JSON.parse(body).Title); 
-		    console.log("Year : "+JSON.parse(body).Year); 
-		    console.log("IMDB Rating : "+JSON.parse(body).imdbRating); 
-		    console.log("Country where the movie was produced : "+JSON.parse(body).Country); 
-		    console.log("Language : "+JSON.parse(body).Language); 
-		    console.log("Plot : "+JSON.parse(body).Plot); 
-		    console.log("Actors : "+JSON.parse(body).Actors); 
-		    console.log("Rotten Tomatoes Rating : "+JSON.parse(body).tomatoRating); 
-		   console.log("Rotten Tomatoes URL : "+JSON.parse(body).tomatoURL); // Show the HTML for the Google homepage. 
-		  }
-		});
-	}
 
 }
 
@@ -140,24 +141,33 @@ function sDetails(sName){
             console.log('Error occurred: ' + err);
             return;
         }else{
-            	var trackArray = data.tracks.items;
-            	trackArray.forEach(function(item,index){
-                    console.log("***********************");
-            		console.log("Album Name: "+trackArray[index].album.name);
-            		console.log("Track Name: "+ trackArray[index].name);
-            		console.log("Artist Name: "+trackArray[index].artists[0].name);
-            		console.log("Preview Url: "+trackArray[index].preview_url);
-            		console.log("-----------------------------------");
-                   });
+        	var trackArray = data.tracks.items;
+        	trackArray.forEach(function(item,index){
+                console.log("***********************");
+        		console.log("Album Name: "+trackArray[index].album.name);
+        		console.log("Track Name: "+ trackArray[index].name);
+        		console.log("Artist Name: "+trackArray[index].artists[0].name);
+        		console.log("Preview Url: "+trackArray[index].preview_url);
+        		console.log("-----------------------------------");
+            });
                     
                 
-            }
+        }
 
         
-        	});
+    });
         
 }
 
+function usageNotes() {
+	console.log("please give valid input");
+	console.log("use the following commands to get the information you want");
+	console.log("node liri.js my-tweets ");
+	console.log("node liri.js spotify-this-song '<song name here>' ");
+	console.log("node liri.js movie-this '<movie name here> ");
+	console.log("node liri.js do-what-it-says ");
+	console.log("node liri.js help ");
+}
 
 switch(expression){
 	case "my-tweets":
@@ -172,7 +182,11 @@ switch(expression){
 	case  "do-what-it-says":
 			doWhatItSays();
 			break;
+	case  "help":
+			usageNotes();
+			break;
 	default:
-			console.log("please give valid input");
+			usageNotes();
 }
+
  
